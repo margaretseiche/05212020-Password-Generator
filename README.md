0c0cdd46f4695ef82b373650e54166408885e47f
Margaret Seiche <margaretseiche@hotmail.com>
2020-05-22 21:00:12 -0400

    finished generating password but still error as undefined

--- README.md
+++ README.md
@@ -15,7 +15,8 @@ I set the following variables.
     var number = ["0","1","2","3","4","5","6","7","8","9"];
     var special = ["!","@","#","$","%","^","*","?"];
 
-    To confirm that the arrays were input correctly (for instance not skipping a letter in the alphabet), I console.logged them and the array lengths.
+    To confirm that the arrays were input correctly (for instance not skipping a letter in the alphabet), I console.logged 
+    them and the array lengths.
 
     I also created an empty array as the holder for whichever character types the user selects.
         var pooledArray = [];
@@ -28,17 +29,22 @@ So I went back to start the
 
     First, I used a series of prompts (with while statement) to get the desired password length.
         //ATTEMPTED 2 WHILE STATEMENTS, ONE ABOUT LENGTH AND ONE ABOUT 'NOT A NUMBER'.  
-        //BUT IT DOES NOT LOOP.  SO COMBINED WHILE CONDITIONS.
+        //BUT IT DOES NOT LOOP.  SO I COMBINED THE WHILE CONDITIONS.
 
         //IF SOMEONE INPUT H, THAT FAILS AND THERE WOULD BE A PROMPT TO CORRECT IT.  BUT THEN THAT WOULD BE IN SECOND WHILE STATEMENT.
         //IN THAT CASE, THE FIRST WHILE CONDITION WAS ALREADY PASSED (AND NOT LOOPED TO CHECK AGAIN), 
         //SO THERE IS NOTHING PREVENTING INPUT OF A NUMBER THAT IS TOO BIG OR TOO SMALL.
+        
+        //REMOVED FOLLOWING WHILE STATEMENT
         //while(isNaN(passwordLength)) {
         //var passwordLengthNotNumber = alert ("That is not a number.");
         //passwordLength = prompt("Please pick a number between 8 and 128.");    
         //}  
 
-
     Then, there were a few confirm questions about whether the user wants to include each type of character.    
 
     I used the responses to set the array that would be used as the pool for generating a password.  To start, the pooled array was empty.I used if statements to confirm each character type and concatenated the respective array into pooledArray if the user wanted to include the specific set.  I console.logged the confirm responses and the final array to verify that everything worked.  
+
+    Then I completed the inner function to actually generate the password.  I ran a for loop for the length of the password.  Within the loop, I used a random number generator and pulled a value from pooledArray using that random number (during each loop) as the index (where to pull from the array).  I console.logged it and the password appears, but it has an 'undefined' error coming up.  I tried to convert to a string, but it still comes back as an error.
+
+    The intention with the final two lines in Javascript was to have the random password appear on screen in the white section of the box. 
--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // setting variables --- Initial password length set it zero to initialize it as a number.
-var passwordLength = 0;
+var passwordLength = "0";
 
 // arrays set for possible password components
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -11,21 +11,22 @@ var number = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","@","#","$","%","^","*","?"];
 var pooledArray = [];
 
-// confirm array details
-//console.log(lowercase.length);
-//console.log(uppercase.length);
-//console.log(number.length);
-//console.log(special.length);
+        // confirm array details
+        console.log("Lowercase" + lowercase.length);
+        console.log("uppercase" + uppercase.length);
+        console.log("number" + number.length);
+        console.log("special" + special.length);
 
-//console.log(lowercase);
-//console.log(uppercase);
-//console.log(number);
-//console.log(special);
+        console.log(lowercase);
+        console.log(uppercase);
+        console.log(number);
+        console.log(special);
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-
 
   var passwordLength = prompt("Passwords can be between 8 and 128 characters.  How long would you like your password to be?");
       
@@ -39,13 +40,13 @@ function writePassword() {
   var numberPrompt = confirm("Would you like to include numbers?");
   var specialPrompt = confirm("Would you like to include special characters (like punctuation marks)?");
 
-  while(lowercasePrompt === false && uppercasePrompt === false && numberPrompt === false && specialPrompt === false) {
-    var universalPrompt = alert("You must select one of the character types to generate a password.");
-    var lowercasePrompt = confirm("Would you like to include lowercase letters?");
-    var uppercasePrompt = confirm("Would you like to include uppercase letters?");
-    var numberPrompt = confirm("Would you like to include numbers?");
-    var specialPrompt = confirm("Would you like to include special characters (like punctuation marks)?");
-  }
+        while(lowercasePrompt === false && uppercasePrompt === false && numberPrompt === false && specialPrompt === false) {
+          var universalPrompt = alert("You must select one of the character types to generate a password.");
+          var lowercasePrompt = confirm("Would you like to include lowercase letters?");
+          var uppercasePrompt = confirm("Would you like to include uppercase letters?");
+          var numberPrompt = confirm("Would you like to include numbers?");
+          var specialPrompt = confirm("Would you like to include special characters (like punctuation marks)?");
+        }
 
   if(lowercasePrompt === true) {
     pooledArray = pooledArray.concat(lowercase); 
@@ -60,12 +61,12 @@ function writePassword() {
     pooledArray = pooledArray.concat(special);
   }
 
-  //console.log(lowercasePrompt);
-  //console.log(uppercasePrompt); 
-  //console.log(numberPrompt); 
-  //console.log(specialPrompt);
-  
-  //console.log(pooledArray);
+          console.log(lowercasePrompt);
+          console.log(uppercasePrompt); 
+          console.log(numberPrompt); 
+          console.log(specialPrompt);
+          
+          console.log(pooledArray);
 
   var password = generatePassword();
   var passwordFormation = "";
@@ -77,23 +78,15 @@ function writePassword() {
         
         passwordFormation = passwordFormation + pooledArray[random];
 
-        //console.log(passwordFormation);
+        passwordFormation = passwordFormation.toString();
+
+        ////STILL AN ERROR --- RANDOM PASSWORD GENERATES AND CONSOLE.LOGS, BUT SHOWS UNDEFINED BEFORE PASSWORD STRING   
+        console.log(passwordFormation);
       }
-    
-
-
+ 
     }
 }
 
-
-//  var passwordText = document.querySelector("#password");
-//  passwordText.value = password;    
-
-
-  //////////////////THEN a password is generated that matches the selected criteria
-  //////////////////WHEN the password is generated
-  //////////////////THEN the password is either displayed in an alert or written to the page
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//////////////////THEN the password is either displayed in an alert or written to the page
+var passwordText = document.querySelector("#password");
+passwordText.value = password;    
