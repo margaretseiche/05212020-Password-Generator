4a6aee3eecf8b3c090c01627489f078e91bdd2b8
Margaret Seiche <margaretseiche@hotmail.com>
2020-05-28 22:22:36 -0400

    fixed error with special character-only option

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var number = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","@","#","$","%","^","*","?"];
-var pooledArrayObject = {};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -31,7 +30,7 @@ function writePassword() {
   var specialPrompt = confirm("Would you like to include special characters (like punctuation marks)?");
 
       while(lowercasePrompt === false && uppercasePrompt === false && numberPrompt === false && specialPrompt === false) {
-        var universalPrompt = alert("You must select one of the character types to generate a password.");
+        alert("You must select one of the character types to generate a password.");
         var lowercasePrompt = confirm("Would you like to include lowercase letters?");
         var uppercasePrompt = confirm("Would you like to include uppercase letters?");
         var numberPrompt = confirm("Would you like to include numbers?");
@@ -48,23 +47,21 @@ function writePassword() {
       for (var i = 0; i < passwordLength; i++) {
         if(lowercasePrompt === true && finalPassword.length < passwordLength) {
           var random = Math.floor(Math.random() * lowercase.length);
-          finalPassword = finalPassword + lowercase[random];
+          finalPassword += lowercase[random];
         }
         if (uppercasePrompt === true && finalPassword.length < passwordLength) {
           var random = Math.floor(Math.random() * uppercase.length);
-          finalPassword = finalPassword + uppercase[random];
+          finalPassword += uppercase[random];
         }              
         if (numberPrompt === true && finalPassword.length < passwordLength) {
           var random = Math.floor(Math.random() * number.length);
-          finalPassword = finalPassword + number[random];
+          finalPassword += number[random];
         }          
         if (specialPrompt === true && finalPassword.length < passwordLength) {  
           var random = Math.floor(Math.random() * special.length);
-          finalPassword = finalPassword + special[random];
-        }
-        else if (finalPassword.length === passwordLength) {
-          return finalPassword;
-        }  
+          finalPassword += special[random];
+        }       
       }
+      return finalPassword; 
     }
 }
